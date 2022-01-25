9c3468cf9b9f34b3630cdb71cf0101d5d3e4901b
Farid Ahmed <w3farid@gmail.com>
2022-01-25 22:37:46 +0600

    update test case

--- fileserver/src/test/java/com/futureaisoft/FileserverApplicationTests.java
+++ fileserver/src/test/java/com/futureaisoft/FileserverApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FileserverApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
