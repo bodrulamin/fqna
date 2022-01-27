d968cd16e8f9be5bd550fde582a2e7fcfbbe57dc
bodrulamin <bodrulaminiu@gmail.com>
2022-01-27 09:33:27 +0600

    m

--- quiz/src/main/java/com/futureaisoft/controller/QuestionController.java
+++ quiz/src/main/java/com/futureaisoft/controller/QuestionController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.model.Product;
 import com.futureaisoft.model.Question;
 import com.futureaisoft.service.QuestionService;
 
