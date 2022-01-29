97d25f1448032bdb1a0365e18355d9103b317ae7
Amina <aminaemcbd@gmail.com>
2022-01-30 00:06:55 +0600

    body design

--- client-mobile/lib/pages/question_answer_body.dart
+++ client-mobile/lib/pages/question_answer_body.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:expansion_tile_card/expansion_tile_card.dart';
 import 'package:flutter/material.dart';
 
@@ -18,41 +16,37 @@ class _QuestionAnswerBodyState extends State<QuestionAnswerBody> {
   @override
   void initState() {
     super.initState();
-    myList = List.generate(20, (i) => "Item : ${i+1}");
+    myList = List.generate(20, (i) => "Item : ${i + 1}");
     _scrollController.addListener(() {
-      if(_scrollController.position.pixels== _scrollController.position.maxScrollExtent){
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
         _getMoreData();
       }
     });
   }
 
-  _getMoreData(){
+  _getMoreData() {
     print("get more data");
-    for(int i = currentMax; i<currentMax+10; i++){
-      myList.add("Item : ${i+1}");
+    for (int i = currentMax; i < currentMax + 10; i++) {
+      myList.add("Item : ${i + 1}");
     }
-    currentMax =  currentMax + 10;
-    setState(() {
-
-    });
+    currentMax = currentMax + 10;
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-
-
     return ListView.builder(
       controller: _scrollController,
       itemExtent: 80,
-      itemBuilder: (context, i){
+      itemBuilder: (context, i) {
         return QuestionCard();
       },
-      itemCount: myList.length+1 ,
+      itemCount: myList.length + 1,
       //children: [for (int i = 0; i < 10; i++) QuestionCard()],
     );
   }
 }
-
 class QuestionCard extends StatelessWidget {
   const QuestionCard({
     Key? key,
@@ -62,31 +56,31 @@ class QuestionCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
         child: Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Column(
-          children: [
-            Icon(Icons.arrow_upward),
-            Text("8"),
-            Icon(Icons.arrow_downward),
-          ],
-        ),
-        Expanded(
-          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Column(
             children: [
-              Text(""
-                  "updown asdfdsafsdf a fdadsfsdfsdafsadf sdfasdfsdafdsfdsf"
-
-                  ""),
+              Icon(Icons.arrow_drop_up_rounded, size: 23.0),
+              Text("8"),
+              Icon(Icons.arrow_drop_down_rounded, size: 23.0),
             ],
           ),
-        ),
-        Image(
-            image: AssetImage("assets/images/c.png"),
-        height: 30,
-          width: 30,
-        )
-      ],
-    ));
+          Expanded(
+            child: Column(
+              children: [
+                Text(""
+                    "updown asdfdsafsdf a fdadsfsdfsdafsadf sdfasdfsdafdsfdsf"
+                    ""),
+              ],
+            ),
+          ),
+          Image(
+            image: AssetImage("assets/images/226777.png"),
+            height: 30,
+            width: 30,
+          )
+        ],
+      )
+    );
   }
 }
