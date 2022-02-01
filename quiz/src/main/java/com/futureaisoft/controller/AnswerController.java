1517950b7094479df66f573c28a49ebaab2bd411
bodrulamin <bodrulaminiu@gmail.com>
2022-02-01 09:31:03 +0600

    Documentation updated

--- quiz/src/main/java/com/futureaisoft/controller/AnswerController.java
+++ quiz/src/main/java/com/futureaisoft/controller/AnswerController.java
@@ -2,6 +2,8 @@ package com.futureaisoft.controller;
 
 import java.util.List;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,7 +38,32 @@ public class AnswerController {
 
 	private final ApiResponse res = MyConstant.apiRes;
 
+	@GetMapping(value = "")
+	@Operation(summary = "Get Answers", security = @SecurityRequirement(name = "bearerAuth"))
+	public ResponseEntity<ApiResponse> getAnswers(
+			@RequestParam long questionId,
+			@RequestParam(defaultValue = "1") int page,
+			@RequestParam(defaultValue = "20") int size,
+			@RequestParam(defaultValue = "") String q,
+			@RequestParam(defaultValue = "id") String orderby,
+			@RequestParam(defaultValue = "ASC") Sort.Direction direction
+	) {
+		log.info("Starting getAnswers: getAnswers(@RequestParam long page)");
+		try {
+			List<Answer> answers = service.getAnswers(questionId,page - 1, size, q, Sort.by(direction, orderby));
+			res.setStatus(MyConstant.SUCCESS);
+			res.setMessage("Answer loaded successfully ");
+			res.setData(answers);
+			return ResponseEntity.ok(res);
+		} catch (Exception e) {
+			log.trace(e.getMessage(), e);
+			res.setMessage("Answer loading failed!");
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(res);
+		}
+	}
+
 	@PostMapping(value = "")
+	@Operation(summary = "Save Answer", security = @SecurityRequirement(name = "bearerAuth"))
 	public ResponseEntity<ApiResponse> save(@RequestBody Answer entity) {
 
 		log.info("Starting save: save(@RequestBody Answer entity)");
@@ -54,31 +81,11 @@ public class AnswerController {
 		}
 	}
 
-	@GetMapping(value = "")
-	public ResponseEntity<ApiResponse> getAnswers(
-			   @RequestParam long questionId,
-			   @RequestParam(defaultValue = "1") int page,
-	            @RequestParam(defaultValue = "20") int size,
-	            @RequestParam(defaultValue = "") String q,
-	            @RequestParam(defaultValue = "id") String orderby,
-	            @RequestParam(defaultValue = "ASC") Sort.Direction direction
-			) {
-		log.info("Starting getAnswers: getAnswers(@RequestParam long page)");
-		try {
-			List<Answer> answers = service.getAnswers(questionId,page - 1, size, q, Sort.by(direction, orderby));
-			res.setStatus(MyConstant.SUCCESS);
-			res.setMessage("Answer loaded successfully ");
-			res.setData(answers);
-			return ResponseEntity.ok(res);
-		} catch (Exception e) {
-			log.trace(e.getMessage(), e);
-			res.setMessage("Answer loading failed!");
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(res);
-		}
-	}	
 	
     @GetMapping(value = "{id}")
-    public ResponseEntity<ApiResponse> getAnswer(@PathVariable(value = "id") long id) {
+	@Operation(summary = "Get one Answer", security = @SecurityRequirement(name = "bearerAuth"))
+
+	public ResponseEntity<ApiResponse> getAnswer(@PathVariable(value = "id") long id) {
         log.info("Starting getAnswer: getAnswer(@PathVariable(value = \"id\") long id) ");
         try {
             Answer answer = service.getAnswer(id);
@@ -93,7 +100,9 @@ public class AnswerController {
         }
     }
     
-	@DeleteMapping(value = "/{id}")
+	@DeleteMapping(value = "{id}")
+	@Operation(summary = "Delete Answer by answerId", security = @SecurityRequirement(name = "bearerAuth"))
+
 	public ResponseEntity<ApiResponse> delete(@PathVariable(value = "id") Long id) {
 		log.info("Starting delete: delete(@PathVariable(value = \"id\") Long id)");
 
