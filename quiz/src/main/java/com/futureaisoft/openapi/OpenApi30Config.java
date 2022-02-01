b9e670f12b6a4e637f51b6f5526947ed990cfd84
bodrulamin <bodrulaminiu@gmail.com>
2022-02-01 09:21:15 +0600

    Token url added

--- quiz/src/main/java/com/futureaisoft/openapi/OpenApi30Config.java
+++ quiz/src/main/java/com/futureaisoft/openapi/OpenApi30Config.java
@@ -3,11 +3,22 @@ package com.futureaisoft.openapi;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "Question and Answer API", version = "v1"))
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Question and Answer API",
+                version = "v1",
+                license = @License(
+                        url = "http://localhost:9002/oauth2/authorize/google?redirect_uri=http://localhost:3000/oauth2/redirect",
+                        name = "Get Token"
+
+                )
+        )
+)
 @SecurityScheme(
         name = "bearerAuth",
         type = SecuritySchemeType.HTTP,
