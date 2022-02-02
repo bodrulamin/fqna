b82a295a122a2268bb1c8953ddc1d9bc89e27365
mehedihasan03 <mdmehedi03@gmail.com>
2022-02-02 13:24:38 +0600

    delete vote controller, model, repository

--- quiz/src/main/java/com/futureaisoft/repository/VoteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.futureaisoft.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.futureaisoft.model.Vote;
-
-public interface VoteRepository extends JpaRepository<Vote, Long> {
-
-}
--- quiz/src/main/java/com/futureaisoft/service/MyService.java
+++ quiz/src/main/java/com/futureaisoft/service/MyService.java
@@ -22,7 +22,6 @@ import com.futureaisoft.repository.PointChartRepository;
 import com.futureaisoft.repository.QuestionRatingRepository;
 import com.futureaisoft.repository.QuestionRepository;
 import com.futureaisoft.repository.TopicRepository;
-import com.futureaisoft.repository.VoteRepository;
 
 @Service
 public class MyService {
@@ -44,9 +43,6 @@ public class MyService {
 
     @Autowired
     private AnswerRatingRepository answerRatingRepository;
-    
-    @Autowired
-    private VoteRepository voteRepository;
 
     public Question saveQuestion(Question question) throws Exception {
         question = questionRepository.save(question);
